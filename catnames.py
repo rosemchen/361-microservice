2aa088671e7051d661007afcdf56d2b8963e167d
rosemchen <chenro@oregonstate.edu>
2022-05-09 16:44:17 -0700

    initial commit

---
+++ catnames.py
@@ -0,0 +1,30 @@
+# Microservice for Dawn's Cat Avatar Project
+# Reads "run" from catnames.json
+# Prints out object with 10 random cat names
+
+import random
+import time
+
+
+catlist = ["muffins", "cupcake", "flour", "sugar", "pepper",
+             "chocolate", "oatmeal", "cereal", "puffs", "cookie",
+           "brownie", "cannoli", "hershey", "butterscotch", "licorice", "maple", "honey", "mochi", "mousse", "nutmeg", "cinnamon", "smores", "shortcake", "butters", "waffle", "pancake", "toffee", "pumpkin", "marshmallow"]
+
+random.seed()
+while True:
+    time.sleep(3)
+    readcatnames = open('catnames.json', 'r')
+    entry = readcatnames.readline()
+    readcatnames.close()
+    if entry == 'run':
+        break
+
+writecatnames = open('catnames.json', 'w')
+if entry == 'run':
+    randomNames = random.choices(catlist, k = 10)
+    target = {39: None, 91: None, 93: None}
+    randomNamesCleaned = (str(randomNames).translate(target))
+    #randomNamesCleaned = randomNamesCleaned.replace(',','')
+    writecatnames.write(format(randomNamesCleaned))
+writecatnames.close()
+
---
+++ testmicroservice.py
@@ -0,0 +1,40 @@
+# Program that generates psuedo-random numbers (PRNG service)
+
+import random
+import time
+
+random.seed()   #initialize rng
+while True:
+    #print("#21")
+    time.sleep(3)       #sleep for one second
+    #print("#22")
+    prng = open('prng-service.txt', 'r')    #open prng-service.txt file
+    #print("#23")
+    entry = prng.readline()     #read file entry
+    prng.close()
+    if entry == 'run':
+        break
+
+#erase image-service file to clear it
+#image = open('image-service.txt', 'w')
+#imageNum = image.readline()
+
+
+prng1 = open('prng-service.txt', 'w')
+#print("#24")
+#print(entry)
+if entry == 'run':      #if entry file "run"
+    #print("#25")
+    randomNum = str(random.randint(1,3))    #generate random number 1-3
+    #prng.seek(0)
+    #print("#26")
+    #print(randomNum)
+    prng1.write(randomNum)
+prng1.close()
+
+
+
+
+words = ["as", "I", "his", "that", "he", "was", "for", "on", "are", "with"]
+
+cats = [""]
