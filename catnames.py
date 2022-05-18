9738adcf1f15544f3e888055fb14911c44793aff
rosemchen <chenro@oregonstate.edu>
2022-05-17 18:16:41 -0700

    final microservice completion - catnames.py

--- catnames.py
+++ catnames.py
@@ -9,19 +9,20 @@ import json
 
 catlist = ["muffins", "cupcake", "flour", "sugar", "pepper",
              "chocolate", "oatmeal", "cereal", "puffs", "cookie",
-           "brownie", "cannoli", "hershey", "butterscotch", "licorice", "maple", "honey", "mochi", "mousse", "nutmeg", "cinnamon", "smores", "shortcake", "butters", "waffle", "pancake", "toffee", "pumpkin", "marshmallow"]
+           "brownie", "cannoli", "hershey", "butterscotch", "licorice",
+           "maple", "honey", "mochi", "mousse", "nutmeg", "cinnamon",
+           "smores", "shortcake", "butters", "waffle", "pancake",
+           "toffee", "pumpkin", "marshmallow", "bacon", "eggs",
+           "pancake", "oreo", "chip", "mittens", "buttercup", "ginger",
+           "apple", "tuna", "popcorn"]
 
 random.seed()
-while True:
-    readcatnames = open('catnames.json', 'r')
-    entry = readcatnames.readline()
-    readcatnames.close()
-    if entry == 'run':
-        writecatnames = open('catnames.json', 'w')
-        randomNames = random.choices(catlist, k=10)
-        target = {39: None, 91: None, 93: None}
-        catnamedict = defaultdict(list)
-        for names in randomNames:
-            catnamedict["names"].append(names)
-        writecatnames.write(json.dumps(catnamedict))
-        writecatnames.close()
+writecatnames = open('catnames.json', 'w')
+randomNames = random.choices(catlist, k=10)
+target = {39: None, 91: None, 93: None}
+catnamedict = defaultdict(list)
+for names in randomNames:
+    catnamedict["names"].append(names)
+writecatnames.write(json.dumps(catnamedict))
+writecatnames.close()
+exit()
